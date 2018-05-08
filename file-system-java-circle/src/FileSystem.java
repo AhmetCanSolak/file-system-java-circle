b47a81a7ca701375931b12ba8be9263998d04e03
AhmetCanSolak <asolak14@ku.edu.tr>
2018-05-08 14:17:49 +0300

    filesystem class implementation started

--- file-system-java-circle/src/FileSystem.java
+++ file-system-java-circle/src/FileSystem.java
@@ -1,4 +1,24 @@
 
 public class FileSystem {
 	// TODO keep it singleton
+	private String hostOS;
+
+	public FileSystem(String targetFolder) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getHostOS() {
+		return hostOS;
+	}
+
+	public void setHostOS(String hostOS) {
+		this.hostOS = hostOS;
+	}
+
+	public void configure() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
--- file-system-java-circle/src/Runner.java
+++ file-system-java-circle/src/Runner.java
@@ -1,10 +1,26 @@
 
-
 public class Runner {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");		
+		
+		
+		// 1. Get root folder for the filesystem instance and OS type
+		// String targetFolder = args[0];
+		String targetFolder = "hehe";
+		String hostOS = System.getProperty("os.name");
+		
+		// 2. Check if there is a previously created filesystem
+		
+		// 3. Initiate or re-open the filesystem at target folder
+		FileSystem fs = new FileSystem(targetFolder);
+		fs.setHostOS(hostOS);
+		fs.configure();
+		
+		// 4. Start filesystem-CLI
+		// 5. Clear temporaries and exit the program
 		
 		return;
 	}
